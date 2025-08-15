0baadd224e262fe25cb921a9adbd63dca6684ff2
abubakarraja345-source <abubakarraja345@gmail.com>
2025-08-15 12:41:37 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://127.0.0.1:5000/chat';
+const API_URL = '/api/chat';
 function displayPersonality() {
   const personality = localStorage.getItem('chatPersonality') || "Friendly";
   let emoji = "";
@@ -68,6 +68,7 @@ document.getElementById('message').addEventListener('keydown', e => {
 
 
 displayPersonality();
+
 
 
 
